a10c66d7f6f5ca05ac2113939db935bbea0d7df8
Eitan Seri-Levi <eitan@winc.com>
2021-09-21 00:52:23 -0700

    moved root directory

---
+++ contracts/FakeNFT.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract FakeNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("FakeNFT", "FNFT") {
+    }
+
+    function dropNFT(address buyer) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _mint(buyer, newTokenId);
+
+        return newTokenId;
+    }
+}
---
+++ test/end-to-end.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe('End To End', function () {
+    var accounts;
+    var hotDropFactory;
+    var hotDropBroker;
+    var fakeNFT;
+    before(async() => {
+        accounts = await ethers.getSigners();
+        const FakeNFT = await ethers.getContractFactory("FakeNFT");
+        const HotDropFactory = await ethers.getContractFactory("HotDropFactory");
+        const HotDropBroker = await ethers.getContractFactory("HotDropBroker");
+        
+        fakeNFT = await FakeNFT.deploy();
+        await fakeNFT.deployed();
+
+        hotDropFactory = await HotDropFactory.deploy();
+        await hotDropFactory.deployed();
+
+        hotDropBroker = await HotDropBroker.deploy(hotDropFactory.address, accounts[0].address, 1);
+        await hotDropBroker.deployed();
+    })
+    it("Broker contract should have reference to factory contract address", async() => {
+        expect(await hotDropBroker.hotDropFactory()).to.equal(hotDropFactory.address);     
+    });
+    it("New NFT project should have a project id", async() => {
+        expect(await hotDropFactory.createProject(fakeNFT.address, 'test')).to.not.be.empty;
+    });
+    it("NFT project address should be accessible via project ID", async() => {
+        expect(await hotDropFactory.projectIdToTokenAddress(1)).to.not.be.empty;
+    });
+    it("Check if factory contract reverts when trying to create project w/ non nft contract", async() => {
+        await expect(hotDropFactory.createProject(accounts[0].address, 'test')).to.be.reverted;
+    });
+    it("Places an order", async() => {
+        await hotDropBroker.placeOrder(1,1, {
+            value: ethers.utils.parseEther("1.0")
+        });
+        expect(await hotDropBroker.balances(accounts[0].address)).to.not.be.equal(0);
+    });
+    it("Fulfills an order", async() => {
+        await fakeNFT.approve(hotDropBroker.address, 1);
+        await hotDropBroker.fulfillOrder(accounts[0].address, 1, 1, ethers.utils.parseEther("1.0"), accounts[0].address);
+    });
+});
